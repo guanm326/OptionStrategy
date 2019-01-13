8a535b329fb98e18c6a910db31fa4af9da2c4651
Dongli Wang <wangdongli0102@163.com>
2019-01-13 19:32:36 +0800

    [FactorTrading][add alphsa1-2]

--- MachineLearning/example/vol_prediction_example.py
+++ MachineLearning/example/vol_prediction_example.py
@@ -1,12 +1,22 @@
 import datetime
+import numpy as np
 from MachineLearning.data import get_index_mktdata, get_index_intraday
 from MachineLearning.util import Util
 from MachineLearning.model.FactorsTrading import Factors
 dt_start = datetime.date(2018,1,1)
 dt_end = datetime.date(2018,12,31)
 name_code = Util.STR_INDEX_50SH
-df_daily = get_index_mktdata(dt_start,dt_end,name_code)
-# df_minute = get_index_intraday(dt_start,dt_end,name_code)
-print(df_daily)
-alpha1 = Factors.alpha_1(df_daily)
-print(alpha1)
+df_daily = get_index_mktdata(dt_start,dt_end,name_code).sort_values(by=Util.DT_DATE,ascending=False)
+# print(df_daily)
+close = np.array(df_daily[Util.AMT_CLOSE])
+open = np.array(df_daily[Util.AMT_OPEN])
+high = np.array(df_daily[Util.AMT_HIGH])
+low = np.array(df_daily[Util.AMT_LOW])
+volume = np.array(df_daily[Util.AMT_TRADING_VOLUME])
+value = np.array(df_daily[Util.AMT_TRADING_VALUE])
+data_array = [close,open,high,low,volume,value]
+# print(close)
+alpha2 = Factors.alpha_2(data_array)
+print(alpha2)
+alpha3 = Factors.alpha_3(data_array)
+print(alpha3)
--- MachineLearning/model/FactorsTrading.py
+++ MachineLearning/model/FactorsTrading.py
@@ -6,8 +6,27 @@ import numpy as np
 class Factors:
 
     @staticmethod
-    def alpha_1(df:pd.Series):
-        delta = np.log(df[Util.AMT_TRADING_VOLUME]).diff()
+    def alpha_2(data_array,i=0):
+        [close, open, high, low, volume, value] = data_array
+        a0 = ((close[i+0]-low[i+0])-(high[i+0]-close[i+0]))/(high[i+0]-low[i+0])
+        a1 = ((close[i+1]-low[i+1])-(high[i+1]-close[i+1]))/(high[i+1]-low[i+1])
+        delta_a = a0-a1
+        res = -1*delta_a
+        return res
 
-        return
+    @staticmethod
+    def alpha_3(data_array,i=0):
+        [close, open, high, low, volume, value] = data_array
+        res = 0
+        for j in i + np.arange(6):
+            if close[j+0] == close[j+1]:
+                a = 0
+            else:
+                if close[j+0] > close[j+1]:
+                    a = close[j+0] - min(low[j+0],close[j+1])
+                else:
+                    a = close[j+0] - max(high[j+0],close[j+1])
+            res += a
+            print(j,a)
+        return res
 
